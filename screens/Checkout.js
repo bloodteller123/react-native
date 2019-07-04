962a49ef6f838de3e46c5d5cc72f89ab6fa17c7a
Andrew Buist <andrew.jbuist@gmail.com>
2019-07-04 09:04:31 -0400

    made the picker change the elements based on its selection

--- screens/Checkout.js
+++ screens/Checkout.js
@@ -49,35 +49,46 @@ export default class Checkout extends Component {
     super(props);
     this.state = {
       total: this.props.navigation.getParam("total"),
-      CreditPicker: true
+      CreditPicker: "credit",
+      showCredit: true
     };
   }
 
   render() {
-    const CreditPicker = this.state.CreditPicker;
     return (
       <View style={{ padding: 10 }}>
-        <Text>Your Order Total: ${this.state.total}</Text>
+        <Text>
+          Your Order Total: ${this.state.total} {this.state.CreditPicker + ""}{" "}
+          {this.state.showCredit}{" "}
+        </Text>
+
         <Picker
           mode="dropdown"
           style={{ height: 50, width: 200 }}
           selectedValue={this.state.CreditPicker}
           onValueChange={value => {
-            this.setState({ pickerValue: value });
+            this.setState({ CreditPicker: value });
           }}
         >
-          <Picker.Item label="Credit" value="true" key="0" />
-          <Picker.Item label="Debit" value="true" key="1" />
-          <Picker.Item label="Cash" value="false" key="2" />
+          <Picker.Item label="Credit" value="credit" key="0" />
+          <Picker.Item label="Debit" value="debit" key="1" />
+          <Picker.Item label="Cash" value="cash" key="2" />
         </Picker>
-        <Text>Credit/Debit Card Number:</Text>
-        <TextInput id="ccNumber" placeholder="############" />
-        <Text>Expiry Date:</Text>
-        <TextInput id="expDate" placeholder="##/##" />
-        <Text>CVV:</Text>
-        <TextInput id="CVV" placeholder="###" />
-        <Text>Full Name:</Text>
-        <TextInput id="name" placeholder="John Smith" />
+        {"credit" == this.state.CreditPicker ||
+        "debit" == this.state.CreditPicker ? (
+          [
+            <Text>Credit/Debit Card Number:</Text>,
+            <TextInput id="ccNumber" placeholder="############" />,
+            <Text>Expiry Date:</Text>,
+            <TextInput id="expDate" placeholder="##/##" />,
+            <Text>CVV:</Text>,
+            <TextInput id="CVV" placeholder="###" />,
+            <Text>Full Name:</Text>,
+            <TextInput id="name" placeholder="John Smith" />
+          ]
+        ) : (
+          <Text>Cash will be done in-store</Text>
+        )}
         <Button title="Submit" />
       </View>
     );
